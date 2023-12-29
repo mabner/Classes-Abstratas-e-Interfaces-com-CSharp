44cb6708d7403da8bfa00f6580bee110ccd02a13
Marcos Leite <m.abner@live.co.uk>
2023-12-29 00:13:11 -0300

    professor class

---
+++ Models/Professor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classes_Abstratas_e_Interfaces_com_CSharp.Models
+{
+    public class Professor : Pessoa
+    {
+        public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, sou professor e meu salário é {Salario}");
+        }
+    }
+}
