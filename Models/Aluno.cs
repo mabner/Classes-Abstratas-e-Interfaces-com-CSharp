c1c6f0fa17a3c16cf3ed2d880f9c9d1a1fd1bd4a
Marcos Leite <m.abner@live.co.uk>
2023-12-28 23:34:12 -0300

    aluno class

---
+++ Models/Aluno.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classes_Abstratas_e_Interfaces_com_CSharp.Models
+{
+    public class Aluno : Pessoa
+    {
+        public double Nota { get; set; }
+
+        public override void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, e sou aluno nota {Nota}.");
+        }
+    }
+}
