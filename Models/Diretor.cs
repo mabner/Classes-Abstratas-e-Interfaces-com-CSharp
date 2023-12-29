29c0e442e7a46f4c3dd73f76ff2f9ea254accd50
Marcos Leite <m.abner@live.co.uk>
2023-12-29 00:15:18 -0300

    sealed class example

---
+++ Models/Diretor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classes_Abstratas_e_Interfaces_com_CSharp.Models
+{
+    // sealed classes cannot be inherited
+    public sealed class Diretor : Professor
+    {
+
+    }
+}
