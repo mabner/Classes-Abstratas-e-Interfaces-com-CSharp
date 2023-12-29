77a4177a622bcf24c55b26bcb8808fb4a0576cb4
Marcos Leite <m.abner@live.co.uk>
2023-12-29 00:52:24 -0300

    implementing interfaces

---
+++ Interfaces/ICalculadora.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classes_Abstratas_e_Interfaces_com_CSharp.Interfaces
+{
+    public interface ICalculadora
+    {
+        int Somar(int num1, int num2);
+        int Subtrair(int num1, int num2);
+        int Multiplicar(int num1, int num2);
+        int Dividir(int num1, int num2);
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Classes_Abstratas_e_Interfaces_com_CSharp.Interfaces;
+
+namespace Classes_Abstratas_e_Interfaces_com_CSharp.Models
+{
+    // implements the ICalculadora interface
+    public class Calculadora : ICalculadora
+    {
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Somar(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
